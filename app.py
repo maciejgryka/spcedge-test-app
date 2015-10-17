5922dab4aa03b9f60f5ac22781f1f824c2c15653
Maciej Gryka <maciej.gryka@gmail.com>
2015-10-17 17:24:00 +0200

    added a thing to test numpy installation

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 import os
+import sys
+from cStringIO import StringIO
 
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-app.debug = os.getenv('DEBUG', False)
+app.debug = os.getenv('DEBUG', True)
 
 
 @app.route('/')
 def home():
-    return 'There is nothing here.'
+    return render_template('index.html', content='')
+
+
+@app.route('/test-numpy')
+def test_numpy():
+    old_stdout = sys.stdout
+    old_stderr = sys.stderr
+    sys.stdout = sys.stderr = test_output = StringIO()
+
+    import numpy as np
+    np.test()
+
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    return '<pre>{0}</pre>'.format(test_output.getvalue())
 
 
 if __name__ == '__main__':
